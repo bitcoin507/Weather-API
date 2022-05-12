32de0b9bb678388c7b3901dd1b7e2c68608b3d34
Craig <panamaworldwideinvestments@gmail.com>
2022-05-12 17:51:27 +0100

    adding html,javascript and css files

---
+++ assets/Javascript/script.js
@@ -0,0 +1,26 @@
+const APIKey = 'd69762104d76621b9eda338e1a084d6a';
+
+let city = 'Toronto';
+
+ 
+
+
+//const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + 'city' +'&appid='+ 'd69762104d76621b9eda338e1a084d6a' +'&units=metric';
+
+
+let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + 'city' +'&appid='+ 'd69762104d76621b9eda338e1a084d6a' +'&units=metric',requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+    
+
+
+
+
+
