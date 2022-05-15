f2252774f706982a3289978062cb833df7728a5f
Craig <panamaworldwideinvestments@gmail.com>
2022-05-15 14:45:40 +0100

    api call now functions and dumps city current weather on click

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,13 +1,18 @@
 const APIKey = 'd69762104d76621b9eda338e1a084d6a';
 
+const searchButton = document.getElementById('searchButton');
+searchButton.addEventListener('click',searchCity);
 
+  function searchCity() {
+    let city = document.getElementById('myText').value;
+    const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid='+  + APIKey +'&units=metric';
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid='+ APIKey +'&units=metric')
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 
+  }
 
-
-  
-
-
-  let city = document.getElementById('myText').value;
   
   
 
@@ -15,7 +20,7 @@ const APIKey = 'd69762104d76621b9eda338e1a084d6a';
  
 
 
-const queryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid='+  + APIKey +'&units=metric';
+
 
 
 
